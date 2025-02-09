e3fc43d4b0a2e1f6efa3293d8ab9fe80bbae0b5a
Vikram <vikramji283@gmail.com>
2025-02-09 18:42:04 +0530

    admin login page

--- src/app/auth/admin-login/admin-login.component.ts
+++ src/app/auth/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-login',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent {
+  loginForm: FormGroup;
+  isSubmitted = false;
+  errorMessage = '';
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get formControls() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+
+    if (username === 'admin' && password === 'admin123') {
+      window.open('https://www.google.com', '_self'); // Redirect for testing
+    } else {
+      this.errorMessage = 'Invalid username or password';
+    }
+  }
 }
